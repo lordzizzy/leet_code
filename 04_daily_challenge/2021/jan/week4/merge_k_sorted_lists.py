609466e22e0c508ac7f695587a3967645e303a55
zen.chong <lordzizzy@gmail.com>
2021-01-25 15:43:06 +0800

    implement fast n log k merge solution for merge k sorted list challenge question

--- 04_daily_challenge/2021/jan/week4/merge_k_sorted_lists.py
+++ 04_daily_challenge/2021/jan/week4/merge_k_sorted_lists.py
@@ -48,20 +48,36 @@ class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         if not lists or len(lists) == 0:
             return None
-        head = lists[0]
-        for i in range(1, len(lists)):
-            head = self.merge2Lists(head, lists[i])
-        return head
-
-    def merge2Lists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        head = l1 if l1.val <= l2.val else l2
-        head.next = self.merge2Lists(
-            l1.next, l2) if l1.val <= l2.val else self.merge2Lists(l1, l2.next)
-        return head
+        
+        # n log K time complexity
+        while len(lists) > 1:
+            merged = []
+            
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                merged.append(self.merge2Lists(l1, l2))
+            lists = merged
+        return lists[0]
+    
+    
+    def merge2Lists(self, l1: ListNode, l2: ListNode):
+        dummy = ListNode()
+        tail = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
 
 
 def toListOfListNodes(input: List[List[int]]) -> List[ListNode]:
