63e656bbc1b9583987df889227a204f675bc0948
zen.chong <lordzizzy@gmail.com>
2021-01-25 16:24:31 +0800

    code indentation fix

--- 04_daily_challenge/2021/jan/week4/merge_k_sorted_lists.py
+++ 04_daily_challenge/2021/jan/week4/merge_k_sorted_lists.py
@@ -48,23 +48,24 @@ class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         if not lists or len(lists) == 0:
             return None
-        
+
         # n log K time complexity
         while len(lists) > 1:
             merged = []
-            
+
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if (i + 1) < len(lists) else None
                 merged.append(self.merge2Lists(l1, l2))
+
             lists = merged
+
         return lists[0]
-    
-    
+
     def merge2Lists(self, l1: ListNode, l2: ListNode):
         dummy = ListNode()
         tail = dummy
-        
+
         while l1 and l2:
             if l1.val < l2.val:
                 tail.next = l1
@@ -73,10 +74,12 @@ class Solution:
                 tail.next = l2
                 l2 = l2.next
             tail = tail.next
+
         if l1:
             tail.next = l1
         if l2:
             tail.next = l2
+
         return dummy.next
 
 
