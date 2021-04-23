e445575aacd5235018be8e898d6b23e51e6612b1
zen.chong <lordzizzy@gmail.com>
2021-04-23 11:59:41 +0800

    add python solution for brick wall

---
+++ 04_daily_challenge/2021/04-apr/week4/brick_wall.py
@@ -0,0 +1,86 @@
+# https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/596/week-4-april-22nd-april-28th/3717/
+
+# There is a brick wall in front of you. The wall is rectangular and has
+# several rows of bricks. The bricks have the same height but different width.
+# You want to draw a vertical line from the top to the bottom and cross the
+# least bricks.
+
+# The brick wall is represented by a list of rows. Each row is a list of
+# integers representing the width of each brick in this row from left to right.
+
+# If your line go through the edge of a brick, then the brick is not considered
+# as crossed. You need to find out how to draw the line to cross the least
+# bricks and return the number of crossed bricks.
+
+# You cannot draw a line just along one of the two vertical edges of the wall,
+# in which case the line will obviously cross no bricks.
+
+# Example:
+# Input: [[1,2,2,1],
+#         [3,1,2],
+#         [1,3,2],
+#         [2,4],
+#         [3,1,2],
+#         [1,3,1,1]]
+
+# Output: 2
+
+# Explanation:
+# # Note:
+
+# The width sum of bricks in different rows are the same and won't exceed
+# INT_MAX.
+
+# The number of bricks in each row is in range [1,10,000]. The height of wall
+# is in range [1,10,000]. Total number of bricks of the wall won't exceed
+# 20,000.
+
+from typing import Callable, DefaultDict, List
+from termcolor import colored
+
+from itertools import accumulate
+
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        dp = DefaultDict[int, int](lambda: 0)       
+        maxEdges = 0
+
+        for row in wall:
+            for idx in accumulate(row[:-1]):
+                dp[idx] += 1
+                maxEdges = max(maxEdges, dp[idx])
+                
+        return len(wall) - maxEdges
+
+
+SolutionFunc = Callable[[List[List[int]]], int]
+
+
+def test_solution(wall: List[List[int]], expected: int) -> None:
+    def test_impl(func: SolutionFunc, wall: List[List[int]], expected: int) -> None:
+        r = func(wall)
+        if r == expected:
+            print(
+                colored(
+                    f"PASSED {func.__name__} => Least bricks path of {wall} is {r}",
+                    "green",
+                )
+            )
+        else:
+            print(
+                colored(
+                    f"FAILED {func.__name__} => Least bricks path of {wall} is {r}, but expected {expected}",
+                    "red",
+                )
+            )
+
+    sln = Solution()
+    test_impl(sln.leastBricks, wall, expected)
+
+
+if __name__ == "__main__":
+    test_solution(
+        wall=[[1, 2, 2, 1], [3, 1, 2], [1, 3, 2], [2, 4], [3, 1, 2], [1, 3, 1, 1]],
+        expected=2,
+    )
