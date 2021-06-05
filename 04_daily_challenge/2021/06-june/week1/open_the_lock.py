87780d947cd0a129086a1253a9917b616e22d147
zen.chong <lordzizzy@gmail.com>
2021-06-05 13:09:03 +0800

    remove over complicated fast solution for open the lock

--- 04_daily_challenge/2021/06-june/week1/open_the_lock.py
+++ 04_daily_challenge/2021/06-june/week1/open_the_lock.py
@@ -86,100 +86,6 @@ class Solution:
                 q.extend(neighbours(node))
         return -1
 
-    def openLock_fast(self, deadends: List[str], target: str) -> int:
-        def int_to_str(n: int) -> str:
-            s = str(abs(n))
-            zero = (4 - len(s)) * "0"
-            return zero + s
-
-        def next_n(current_lock):
-            def dead_test(add_n):
-                nonlocal update_lock
-                idx = -(len(str(abs(add_n))))
-                lock = 10 ** abs(idx + 1) * int(update_lock[idx])
-                temp = lock + add_n
-                temp = int_to_str(temp)
-
-                test_lock = update_lock[:]
-                test_lock[idx] = temp[idx]
-
-                if "".join(test_lock) in deadends:
-                    return False
-                update_lock = test_lock
-                self.count += 1
-                return True
-
-            def add_num(idx):
-                nonlocal update_lock
-                n = update_lock[idx]
-                t = 10 ** abs(idx + 1) if n > "5" else -(10 ** abs(idx + 1))
-                if n == "0":
-                    if update_lock == current_lock:
-                        hold.append(t)  # 暫存其他可以動的選擇
-                        hold.append(-t)
-                    return
-
-                ok = dead_test(t)
-                if not ok:
-                    if update_lock == current_lock:
-                        good_move.append(t)
-                        hold.append(-t)
-                return
-
-            def clean_good_moves():
-                nonlocal good_move
-                while good_move:
-                    pass_ = 0
-                    for n in good_move:
-                        ok = dead_test(n)
-                        if ok:
-                            pass_ += 1
-                            good_move.remove(n)
-                    if pass_ == 0:
-                        break
-                return
-
-            # ----------------function code start
-            # count = 0
-            update_lock = current_lock[:]
-            good_move = []
-            hold = []
-            for i in range(-4, 0):
-                add_n = add_num(i)
-
-            if update_lock != current_lock:
-                clean_good_moves()
-                return update_lock
-
-            # print('not found good match, look for optional matches.')
-            if not hold:
-                return False
-
-            while hold:
-                add_n = hold.pop()
-                ok = dead_test(add_n)
-                if not ok:
-                    continue
-
-                clean_good_moves()
-
-                if update_lock == current_lock:
-                    return False
-                return update_lock
-
-        # ---------------start from target
-        self.count = 0
-        target = list(target)
-        if "0000" in deadends:
-            return -1
-
-        while target != ["0"] * 4:
-            target = next_n(target)
-            if not target:
-                return -1
-
-        return self.count
-
 
 SolutionFunc = Callable[[List[str], str], int]
 
@@ -191,6 +97,7 @@ def test_solution(deadends: List[str], target: str, expected: int) -> None:
         r = func(deadends, target)
         if r == expected:
             print(
+                
                 colored(
                     f"PASSED {func.__name__} => Min moves to open lock of target:{target} with deadends: {deadends} is {r}",
                     "green",
@@ -206,8 +113,6 @@ def test_solution(deadends: List[str], target: str, expected: int) -> None:
 
     sln = Solution()
     test_impl(sln.openLock_bfs, deadends, target, expected)
-    
-    test_impl(sln.openLock_fast, deadends, target, expected)
 
 
 if __name__ == "__main__":
