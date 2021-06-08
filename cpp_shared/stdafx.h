8e55465c88f183f0c037737ebac1f7afd7db3c35
zen.chong <lordzizzy@gmail.com>
2021-06-09 03:18:46 +0800

    use fmt header in standard include

--- cpp_shared/stdafx.h
+++ cpp_shared/stdafx.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#define FMT_HEADER_ONLY
+
 #include "leetcode/format.h"
 
-#include <format>
+#include <fmt/color.h>
+#include <fmt/core.h>
+
 #include <functional>
 #include <iostream>
 #include <string_view>
+
+constexpr auto pass_color = fmt::fg(fmt::color::green);
+constexpr auto fail_color = fmt::fg(fmt::color::red);
