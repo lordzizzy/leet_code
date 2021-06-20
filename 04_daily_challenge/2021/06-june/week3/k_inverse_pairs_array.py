184e89f591acf62d02935f4a8b04fa2b83b71154
zen.chong <lordzizzy@gmail.com>
2021-06-20 17:32:17 +0800

    update python solutions for k inverse pairs array REMEMBER the pitfall of [0] * 4!!!

--- 04_daily_challenge/2021/06-june/week3/k_inverse_pairs_array.py
+++ 04_daily_challenge/2021/06-june/week3/k_inverse_pairs_array.py
@@ -23,14 +23,62 @@
 # 1 <= n <= 1000
 # 0 <= k <= 1000
 
-from typing import Callable
+from functools import lru_cache
+from typing import Callable, List
 
 from termcolor import colored
 
 
 class Solution:
-    # Time complexity: O(N^2 * K)
+    def __init__(self) -> None:
+        self._dp: List[List[int]] = [[-1 for _ in range(1001)] for _ in range(1001)]
+
+    # Time complexity: O(N^2 * N!), Space complexity: O(N)
+    def kInversePairs_brute_force(self, n: int, k: int) -> int:
+        def dfs(num: int, n: int, k: int, lst: List[int]) -> int:
+            # end case after all permutations are generated
+            if num == n + 1:
+                return 1 if has_k_inverse_pairs(k, lst) else 0
+            count = 0
+            for i in range(len(lst) + 1):
+                perm = lst.copy()
+                perm.insert(i, num)
+                count += dfs(num + 1, n, k, perm)
+            return count
+
+        def has_k_inverse_pairs(k: int, lst: List[int]) -> bool:
+            pairs, N = 0, len(lst)
+            for i in range(N):
+                for j in range(i + 1, N):
+                    if lst[i] > lst[j]:
+                        pairs += 1
+            return pairs == k
+
+        return dfs(1, n, k, [])
+
+    # Time complexity: O(N^2 * K), Space complexity: O(N)
     def kInversePairs_dp_topdown(self, n: int, k: int) -> int:
+        def count_inverse_pairs(n: int, k: int) -> int:
+            if n == 0:
+                return 0
+            if k == 0:
+                return 1
+            if self._dp[n][k] >= 0:
+                return self._dp[n][k]
+
+            inv = 0
+            for i in range(min(k, n - 1) + 1):
+                inv += count_inverse_pairs(n - 1, k - i) % int(10e9 + 7)
+
+            self._dp[n][k] = inv
+            return inv
+
+        return count_inverse_pairs(n, k)
+
+    # Same as above but memoization implemented using lru_cache
+    # Time complexity: O(N^2 * K), Space complexity: O(N)
+    def kInversePairs_dp_topdown_2(self, n: int, k: int) -> int:
+        @lru_cache(maxsize=None)
         def dfs(n: int, k: int) -> int:
             if n == 0:
                 return 0
@@ -66,9 +114,15 @@ def test_solution(n: int, k: int, expected: int) -> None:
             )
 
     sln = Solution()
+    test_impl(sln.kInversePairs_brute_force, n, k, expected)
     test_impl(sln.kInversePairs_dp_topdown, n, k, expected)
+    test_impl(sln.kInversePairs_dp_topdown_2, n, k, expected)
 
 
 if __name__ == "__main__":
     test_solution(n=3, k=0, expected=1)
     test_solution(n=3, k=1, expected=2)
+    test_solution(n=3, k=2, expected=2)
+    test_solution(n=1, k=1, expected=0)
+    test_solution(n=4, k=2, expected=5)
+    # test_solution(n=10, k=2, expected=44)
